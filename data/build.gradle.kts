0e4ff62a7d776b81b13677fb3509600f9451c344
BekzhanNurjanov <baconnkl@gmail.com>
2023-07-13 16:41:47 +0600

    migrate to gradle kts, and use buildSrc for dependency

---
+++ data/build.gradle.kts
@@ -0,0 +1,62 @@
+plugins {
+    id(Plugins.AGP.library)
+    kotlin(Plugins.Kotlin.android)
+
+    // Kotlin Symbol Processing
+    id(Plugins.KSP.ksp) version Versions.KSP
+}
+
+android {
+    namespace = Namespaces.data
+
+    compileSdk = AndroidConfig.compileSdk
+
+    defaultConfig {
+        minSdk = AndroidConfig.minSdk
+    }
+
+    buildTypes {
+        getByName(AndroidConfig.release) {
+            isMinifyEnabled = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro"
+            )
+            buildConfigField("String", "BASE_URL", "\"https://api.pexels.com/\"")
+        }
+
+        getByName(AndroidConfig.debug) {
+            buildConfigField("String", "BASE_URL", "\"https://api.pexels.com/\"")
+        }
+    }
+    compileOptions {
+        sourceCompatibility = Options.compileOptions
+        targetCompatibility = Options.compileOptions
+    }
+    kotlinOptions {
+        jvmTarget = Options.kotlinOptions
+    }
+    buildFeatures {
+        buildConfig = true
+    }
+}
+
+dependencies {
+
+    implementation(project(":domain"))
+
+    // Retrofit
+    implementation(Libraries.Retrofit.retrofit)
+    implementation(Libraries.Retrofit.converterMoshi)
+
+    // Moshi
+    implementation(Libraries.Moshi.moshi)
+    implementation(Libraries.Moshi.kotlin)
+
+    // OkHttp
+    implementation(Libraries.OkHttp.bom)
+    implementation(Libraries.OkHttp.okHttp)
+    implementation(Libraries.OkHttp.loggingInterceptor)
+
+    // Paging
+    api(Libraries.Paging.runtime)
+}
