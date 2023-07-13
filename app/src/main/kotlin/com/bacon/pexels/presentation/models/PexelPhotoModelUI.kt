ffb85f4fbd0324b9ce13226ad7f218b9630d3509
BekzhanNurjanov <baconnkl@gmail.com>
2023-07-14 05:54:50 +0600

    creating PexelPhotoModelUI and added mapping from domain model to ui model

---
+++ app/src/main/kotlin/com/bacon/pexels/presentation/models/PexelPhotoModelUI.kt
@@ -0,0 +1,32 @@
+package com.bacon.pexels.presentation.models
+
+import com.bacon.domain.models.PexelPhotoModel
+import com.bacon.pexels.presentation.base.IBaseDiffModel
+
+data class PexelPhotoModelUI(
+    override val id: Int,
+    val width: Int,
+    val height: Int,
+    val url: String,
+    val photographer: String,
+    val photographerUrl: String,
+    val photographer_id: Int,
+    val avgColor: String,
+    val src: SrcModelUI,
+    val liked: Boolean,
+    val alt: String
+) : IBaseDiffModel<Int>
+
+fun PexelPhotoModel.toUI() = PexelPhotoModelUI(
+    id,
+    width,
+    height,
+    url,
+    photographer,
+    photographerUrl,
+    photographer_id,
+    avgColor,
+    src.toUI(),
+    liked,
+    alt
+)
