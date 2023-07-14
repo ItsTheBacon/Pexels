1f1b2f59a1f6c2bf1aebc9a9282671120d190d31
BekzhanNurjanov <baconnkl@gmail.com>
2023-07-14 06:11:38 +0600

    DataMapper for mapping dtos  to domain module

---
+++ data/src/main/kotlin/com/bacon/pexels/data/utils/DataMapper.kt
@@ -0,0 +1,15 @@
+package com.bacon.pexels.data.utils
+
+/**
+ * Base mapper interface
+ *
+ * @param T domain layer model
+ *
+ */
+interface DataMapper<T> {
+
+    /**
+     * Function for map data layer model to domain layer model
+     */
+    fun mapToDomain(): T
+}
