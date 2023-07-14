315250f686387c9b417767fbe82eb9492b868007
BekzhanNurjanov <baconnkl@gmail.com>
2023-07-14 06:01:50 +0600

    setupNavigation, and made full screen in photoViewingFragment

--- app/src/main/kotlin/com/bacon/pexels/presentation/ui/activity/MainActivity.kt
+++ app/src/main/kotlin/com/bacon/pexels/presentation/ui/activity/MainActivity.kt
@@ -1,15 +1,58 @@
 package com.bacon.pexels.presentation.ui.activity
 
 import android.os.Bundle
+import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import by.kirich1409.viewbindingdelegate.viewBinding
 import com.bacon.pexels.R
+import com.bacon.pexels.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
+
+    private lateinit var navController: NavController
+    private val binding by viewBinding(ActivityMainBinding::bind)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setupNavigation()
+        updateUIComponents()
     }
+
+    private fun setupNavigation() {
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment_container) as NavHostFragment
+        navController = navHostFragment.navController
+        binding.toolbarMainActivity.setupWithNavController(navController)
+    }
+
+    private fun updateUIComponents() = with(binding) {
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            when (destination.id) {
+                R.id.photoViewingFragment -> {
+                    toolbarMainActivity.isVisible = false
+                    setValueOfFlagNoLimits(true)
+                }
+
+                R.id.galleryFragment -> {
+                    setValueOfFlagNoLimits(false)
+                    toolbarMainActivity.isVisible = true
+                }
+            }
+        }
+    }
+
+    private fun setValueOfFlagNoLimits(isFullScreen: Boolean) {
+        if (isFullScreen) {
+            window?.addFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+        } else {
+            window.clearFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+        }
+    }
+
 }
