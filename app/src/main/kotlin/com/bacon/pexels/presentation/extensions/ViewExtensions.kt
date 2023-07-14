795d671b6075721a861265dfd0fd04426c4f2982
BekzhanNurjanov <baconnkl@gmail.com>
2023-07-14 15:37:01 +0600

    added lamba function in loadUrl extension for checking isLoadSuccess or not

--- app/src/main/kotlin/com/bacon/pexels/presentation/extensions/ViewExtensions.kt
+++ app/src/main/kotlin/com/bacon/pexels/presentation/extensions/ViewExtensions.kt
@@ -1,6 +1,34 @@
 package com.bacon.pexels.presentation.extensions
 
+import android.graphics.drawable.Drawable
 import android.widget.ImageView
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
 
-fun ImageView.loadUrl(url: String) = Glide.with(this).load(url).centerCrop().into(this)
+fun ImageView.loadUrl(url: String, onLoad: ((isLoadSuccess: Boolean) -> Unit)? = null) =
+    Glide.with(this).load(url).centerCrop().listener(object : RequestListener<Drawable> {
+        override fun onLoadFailed(
+            e: GlideException?,
+            model: Any?,
+            target: Target<Drawable>?,
+            isFirstResource: Boolean
+        ): Boolean {
+            onLoad?.invoke(false)
+            return false;
+        }
+
+        override fun onResourceReady(
+            resource: Drawable?,
+            model: Any?,
+            target: Target<Drawable>?,
+            dataSource: DataSource?,
+            isFirstResource: Boolean
+        ): Boolean {
+            onLoad?.invoke(true)
+            return false;
+        }
+
+    }).into(this)
