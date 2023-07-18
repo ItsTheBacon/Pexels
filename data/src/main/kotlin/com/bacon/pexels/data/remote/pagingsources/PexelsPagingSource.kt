4b3dcc4a8e34f7e67f79c27ce486d307e51d76b8
BekzhanNurjanov <baconnkl@gmail.com>
2023-07-18 18:48:40 +0600

    changes imports

--- data/src/main/kotlin/com/bacon/pexels/data/remote/pagingsources/PexelsPagingSource.kt
+++ data/src/main/kotlin/com/bacon/pexels/data/remote/pagingsources/PexelsPagingSource.kt
@@ -1,9 +1,9 @@
 package com.bacon.pexels.data.remote.pagingsources
 
-import com.bacon.domain.models.PexelPhotoModel
+import com.bacon.domain.models.photos.PexelPhotoModel
 import com.bacon.pexels.data.base.BasePagingSource
 import com.bacon.pexels.data.remote.apiservices.PexelsApiService
-import com.bacon.pexels.data.remote.dtos.PexelPhotoDto
+import com.bacon.pexels.data.remote.dtos.photos.PexelPhotoDto
 
 class PexelsPagingSource(
     private val apiService: PexelsApiService,
